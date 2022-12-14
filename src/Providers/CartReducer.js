8c9b4d8eda91997738f4c56f0617ec98c261ced4
mmd <mmdslv7707@gmail.com>
2022-12-14 15:13:32 +0330

    edit CartReducer

--- src/Providers/CartReducer.js
+++ src/Providers/CartReducer.js
@@ -1,34 +1,40 @@
+const addProductToCart=(state,payload)=>{
+    const updatedCart=[...state.cart];
+    const updatedIndex=updatedCart.findIndex(item => item.id ===payload.id)
+    if (updatedIndex < 0) {
+        updatedCart.push({...payload,quntity:1})
+    }else{
+        const updatedItem={...updatedCart[updatedIndex]}
+        updatedItem.quntity++;
+        updatedCart[updatedIndex]=updatedItem
+    }
+    return {...state, cart:updatedCart,total:state.total + payload.price}
+
+   
+};
+const removFromCart=(state,payload)=>{
+    const updatedCart=[...state.cart];
+    const updatedIndex=updatedCart.findIndex(item => item.id ===payload.id
+    );
+    const updatedItem={...updatedCart[updatedIndex]}
+    if(updatedItem.quntity === 1){
+       const filterCart= updatedCart.filter((item)=> item.id !==payload.id)
+        return {...state, cart:filterCart,total:state.total - payload.price}
+    }else{
+        const updatedItem={...updatedCart[updatedIndex]}
+        updatedItem.quntity--;
+        updatedCart[updatedIndex]=updatedItem
+        return {...state, cart:updatedCart,total:state.total - payload.price}
+    }
+}
+
+
 const CartReducer=(state,action)=>{
 switch(action.type){
-    case "ADD_TO_CART":{
-        const updatedCart=[...state.cart];
-        const updatedIndex=updatedCart.findIndex(item => item.id ===action.payload.id)
-        if (updatedIndex < 0) {
-            updatedCart.push({...action.payload,quntity:1})
-        }else{
-            const updatedItem={...updatedCart[updatedIndex]}
-            updatedItem.quntity++;
-            updatedCart[updatedIndex]=updatedItem
-        }
-        return {...state, cart:updatedCart,total:state.total + action.payload.price}
-
-       };
-    case "REMOVE_PRODUCT":
-       {
-        const updatedCart=[...state.cart];
-        const updatedIndex=updatedCart.findIndex(item => item.id ===action.payload.id
-        );
-        const updatedItem={...updatedCart[updatedIndex]}
-        if(updatedItem.quntity === 1){
-           const filterCart= updatedCart.filter((item)=> item.id !==action.payload.id)
-            return {...state, cart:filterCart,total:state.total - action.payload.price}
-        }else{
-            const updatedItem={...updatedCart[updatedIndex]}
-            updatedItem.quntity--;
-            updatedCart[updatedIndex]=updatedItem
-            return {...state, cart:updatedCart,total:state.total - action.payload.price}
-        }
-       }
+    case "ADD_TO_CART":return addProductToCart(state,action.payload)
+       
+    case "REMOVE_PRODUCT":return removFromCart(state,action.payload)
+       
         default:
         return state
     }
